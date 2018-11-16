733eb3d73dadfc283cbe1ce0a81f50a77979516b
Pouya Pourbaba <p.pourbaba@gmail.com>
2018-11-16 19:49:19 +0200

    get command line args

--- network.py
+++ network.py
@@ -2,4 +2,9 @@ import socket
 import sys
 
 def main():
-    print('hello git')
+
+    # use the command line args to get the server ip and port
+    if len(sys.argv) > 2:
+        host = sys.argv[1]
+        port = sys.argv[2]
+        port = int(port)
