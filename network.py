07b475b83e6d62fcb12eb1ed787a8865b7f73e82
Pouya Pourbaba <p.pourbaba@gmail.com>
2018-12-02 14:53:31 +0200

    corrected the udp, exchange messages, but ugly code

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 import sys
 
 def main():
@@ -7,39 +8,82 @@ def main():
     port = 10000
 
     # initialize the TCP socket
-    sock_tcp = socket.socket()
-    sock_tcp.connect((host, port))
+    tcp_sock = socket.socket()
+    tcp_sock.connect((host, port))
 
     # message to be sent to the server via TCP, in byte strings
-    message_tcp = 'HELLO\r\n'
-    message_tcp = message_tcp.encode('UTF-8')
-    sock_tcp.send(message_tcp)
+    tcp_msg = 'HELLO\r\n'
+    tcp_msg = tcp_msg.encode('UTF-8')
+    tcp_sock.send(tcp_msg)
 
     # receive the data with a buffer of 1024 bytes
-    response_tcp = sock_tcp.recv(1024)
-    response_tcp = response_tcp.decode('UTF-8')
+    tcp_rsp = tcp_sock.recv(1024)
+    tcp_rsp = tcp_rsp.decode('UTF-8')
 
     # tokenize the data to extract the identity token and the UDP port
-    tokens = response_tcp.split(' ')
+    tokens = tcp_rsp.split(' ')
     id_token = tokens[1]
     udp_port = tokens[2]
 
-    sock_tcp.close()
+    tcp_sock.close()
 
 
     # initialize the UDP socket
-    sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_address = (host, int(udp_port))
 
-    # UDP message
-    message_udp = "Hello from " + id_token + "\r\n"
-    message_udp = message_udp.encode('UTF-8')
-    sock_udp.sendto(message_udp, server_address)
+    # UDP message and packing it in the right structure
+    # cid: char[8] , ack: Bool, eom: Bool[8], data_remaining: unsigned short, content_length: unsigned short, content: char[64]
 
-    # receive data
-    response_udp, server = sock_udp.recvfrom(4096)
+    # client's id token
+    cid = id_token
+    # acknowledgment
+    ack = True
+    eom = False
+    # length of the data still remaining when using multipart messages
+    data_remaining = 0
+    # message content
+    content = ("Hello from " + id_token + "\r\n").encode('UTF-8')
+    # length of the message
+    content_length = len(content)
 
-    sock_udp.close()
+    # packing the udp message and sending it to the server
+    udp_msg = struct.pack('!8s??HH64s', cid.encode('utf-8'), ack, eom, data_remaining, content_length, content)
+    udp_sock.sendto(udp_msg, server_address)
+
+    # receive the data with a buffer of 1024 bytes
+    udp_rsp, sender = udp_sock.recvfrom(1024)
+    udp_rsp_unpacked = struct.unpack('8s??HH64s', udp_rsp)
+
+    # getting the words sent by the server
+    words = udp_rsp_unpacked[5]
+    words = words.decode('UTF-8')
+    print(words)
+    # putting the words in an array
+    word_list = words.split(' ')
+    print(word_list)
+
+    # reversing the list of the words
+    word_list.reverse()
+    print(word_list)
+
+    new_msg = ""
+    for i in word_list:
+        new_msg = new_msg + i.rstrip('\0') + ' '
+    new_msg = new_msg.strip()
+    print(word_list[0])
+    print(len(word_list[0].rstrip('\0')))
+
+    # sending the words in reverse order
+    udp_msg = struct.pack('!8s??HH64s', cid.encode('utf-8'), ack, eom, data_remaining, len(new_msg), new_msg.encode('utf-8'))
+    udp_sock.sendto(udp_msg, server_address)
+    # receive the data with a buffer of 1024 bytes
+    udp_rsp, sender = udp_sock.recvfrom(1024)
+    udp_rsp_unpacked = struct.unpack('8s??HH64s', udp_rsp)
+
+    print(udp_rsp_unpacked)
+    udp_sock.close()
+
 
 if __name__ == '__main__':
     main()
